ac8e6386dc9d5fa9375ef65dcae65c4cf7ebcdfe
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-11-17 18:55:42 +0900

    room index back end finished

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,11 @@
 class RoomsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index]
   def index
-    @rooms = Room.all
+    @hotel = Hotel.find(params[:hotel_id])
+    @rooms = Room.where(hotel_id: params[:hotel_id])
+    if params[:category_id]
+      @rooms = @rooms.where(room_category_id: params[:category_id])
+    end
+    # @rooms = Room.all
   end
 end
